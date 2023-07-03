21d27ddb8ce7adb53631aac15d58fd1c3a66c640
DaurenE <135440638+DaurenE@users.noreply.github.com>
2023-07-03 16:47:27 +0600

    Обновил версию кода для 8 класса

--- physics_for_8.cpp
+++ physics_for_8.cpp
@@ -6,7 +6,7 @@ using namespace std;
 void temprature();
 
 int main() {
-    cout << "Температура:0, \n";
+    cout << "Перевод в температуру:0, ";
 	while (true) {
 		string choose;
 
@@ -20,38 +20,42 @@ int main() {
         switch (stoi(choose)) {
             case 0:
                 cout << "'Температура'\n";
-                temprature();
+                translation_to_temprature();
+                break;
+            case 1:
+                cout << "'теплота'\n";
+                warm();
                 break;
         }
 	}
 }
 
-void temprature () {
+void translation_to_temprature () {
     string SI_temprature;
     float celsius, farengeit, kelvin;
-    cout << "Enter from what to what you want to convert:\nCelsius in Kelvin:01\nKelvin in Celsius:02\nFarengeit in Kelvin:03\nKelvin in Farengeit:04\nCelsius in Farengeit:05" << endl;
+    cout << "Enter from what to what you want to convert:\nCelsius in Kelvin:001\nKelvin in Celsius:002\nFarengeit in Kelvin:003\nKelvin in Farengeit:004\nCelsius in Farengeit:005" << endl;
     cin >> SI_temprature;
-    if (SI_temprature == "01") {
+    if (SI_temprature == "001") {
         cout << "Enter temprature in celsius: ";
         cin >> celsius;
         float temprature_c_k = celsius + 273.15;
         cout << "Answer is: " << temprature_c_k << endl;
-    } else if (SI_temprature == "02") {
+    } else if (SI_temprature == "002") {
         cout << "Enter temprature in Kelvin: ";
         cin >> kelvin;
         float temprature_k_c = kelvin - 273.15;
         cout << "Answer is: " << temprature_k_c << endl;
-    } else if (SI_temprature == "03") {
+    } else if (SI_temprature == "003") {
         cout << "Enter temprature in Farengeit: ";
         cin >> farengeit;
         float temprature_f_k = (farengeit-32)*5/9 + 273.15;
         cout << "Answer is: " << temprature_f_k << endl;
-    } else if (SI_temprature == "04") {
+    } else if (SI_temprature == "004") {
         cout << "Enter temprature in Kelvin: ";
         cin >> kelvin;
         float temprature_k_f = (kelvin-273.15)*9/5 + 32;
         cout << "Answer is: " << temprature_k_f << endl;
-    } else if (SI_temprature == "05") {
+    } else if (SI_temprature == "005") {
         cout << "Enter temprature in celsius: ";
         cin >> celsius;
         float temprature_c_f = (celsius*9/5) + 32;
@@ -59,4 +63,29 @@ void temprature () {
     } else {
         cout << "4o4" << endl;
     }
+}
+
+//define warm func
+void warm () {
+    float Q, m, C, c, T, answer;
+    string unknown;
+    cout << "Enter unknown value (C, Q, T, c, m): ";
+    cin >> unknown;
+    if (unknown == "C") {
+        cout << "Enter Q, △T:\n";
+        cin >> Q >> T;
+        answer = Q/T;
+        cout << "Analisys: C = Q/△T\nAnswer is: " << answer << "[Дж/˚С]\n";
+    } else if (unknown == "c") {
+        cout << "Enter Q, △T, m:\n";
+        cin >> Q >> T >> m;
+        answer = Q/T*m;
+        cout << "Analisys: c = Q/△T*m\nAnswer is: " << answer << "[Дж/˚С*кг]\n"; 
+    } else if (unknown == "Q") {
+        cout << "Enter c, m, △T: \n";
+        cint >> c >> m >> T;
+        answer = c*m*T;
+        cout << "Analisys: Q = c*m*△T\nAnswer is: " << answer << "[Дж]\n";
+    } else if ()
+
 }
