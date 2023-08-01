5e5d6712b8599e33cee2fab317156907cebe3a82
Dauren <dauren.erbolat@gmail.com>
2023-08-01 18:18:14 +0600

    edit auth

--- auth.cpp
+++ auth.cpp
@@ -6,22 +6,21 @@ using namespace std;
 void authorization() {
     string email, user_name, password, symbol;
 
+    size_t found_item = email.find_first_of(symbol);
     symbol = '@';
     cout << "Enter Email (example.email@gmail.com): ";
     cin >> email;
-    cout << "\nEnter user name: ";
-    cin >> user_name;
-    cout << "\nEnter password: ";
-    cin >> password;
-
-    size_t found_item = email.find_first_of(symbol);
-    
     if (found_item != string::npos) {
         cout << "access is allowed" << '\n';
     } else {
         cout << "Try again" << '\n';
     }
+    cout << "\nEnter user name: ";
+    cin >> user_name;
+    
     size_t length = password.length();
+    cout << "\nEnter password: ";
+    cin >> password;
 
     if (length < 8) {
         cout << "password have to be more than 7" << '\n';
